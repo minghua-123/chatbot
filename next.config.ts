b329a9088c44e55e07da161a277ead909c4e0c71
ziho7 <78910421@qq.com>
2025-01-06 11:05:26 +0800

    feat deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslintConfig: {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off'
+    }
+  }
 };
 
 export default nextConfig;
